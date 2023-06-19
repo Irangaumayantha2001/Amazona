f42bec40d88ad485777f22d39cc7cefaf7c6c6b4
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-19 12:10:28 +0530

    update-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
+/** @format */
+
 import data from "./data";
 
 function App() {
   return (
     <div>
       <header>
-      <a href='/'>amazona</a>
+        <a href="/">amazona</a>
       </header>
       <main>
         <h1>Featured Products</h1>
         <div className="products">
-        {
-          data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <img src={product.image} alt={product.name}/>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-          ))
-        }
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
     </div>
