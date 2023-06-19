eef7c728ca5cf1a80c37353ac39edc9ce6fc2b93
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-19 11:58:39 +0530

    update and add css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,17 @@ function App() {
       </header>
       <main>
         <h1>Featured Products</h1>
+        <div className="products">
         {
-          data.products.map(product => (<div>
+          data.products.map((product) => (
+          <div className="product" key={product.slug}>
             <img src={product.image} alt={product.name}/>
-          </div>))
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+          ))
         }
+        </div>
       </main>
     </div>
   );
