3e4c27272612876c1978b3197bfe02357a7a7a98
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-19 11:42:20 +0530

    add data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,10 +1,55 @@
 const data ={
+
     products:[
         {
             name : "Nike Slim Shirt",
             slug: "nike-slim-shirt",
-            image:""
-            price:""
-        }
-    ]
-}
+            category:"shirt",
+            image:"../src/assets/p1.jpg",
+            price: 120,
+            countInStock:10,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            name : "Adidas Fit Shirt",
+            slug: "adidas-fit-shirt",
+            category:"shirt",
+            image:"../src/assets/p2.jpg",
+            price: 250,
+            countInStock:20,
+            brand:"Adidas",
+            rating:4.0,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            name : "Nike Slim pant",
+            slug: "nike-slim-pant",
+            category:"pant",
+            image:"../src/assets/p3.jpg",
+            price: 25,
+            countInStock:15,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:14,
+            description:"high quality product"
+        },
+        {
+            name : "Nike Slim Shirt",
+            slug: "nike-slim-shirt",
+            category:"shirt",
+            image:"../src/assets/p4.jpg",
+            price: 65,
+            countInStock:5,
+            brand:"Puma",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality shirt"
+        },
+    ],
+};
+
+export default data;
