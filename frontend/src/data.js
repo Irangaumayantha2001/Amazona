e6380ba0b08f32b91acbac83706948239bddc7df
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-19 11:30:42 +0530

    add images

---
+++ frontend/src/data.js
@@ -0,0 +1,10 @@
+const data ={
+    products:[
+        {
+            name : "Nike Slim Shirt",
+            slug: "nike-slim-shirt",
+            image:""
+            price:""
+        }
+    ]
+}
